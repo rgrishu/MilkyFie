9a84f0954bfaccd30247399be331df6e6c4c79a5
np4652 <np4652@gmail.com>
2022-03-18 00:50:35 +0530

    reduce ajax call in datatble

--- Milkyfie/wwwroot/js/coreLib.js
+++ Milkyfie/wwwroot/js/coreLib.js
@@ -833,7 +833,7 @@ function ajaxFormSubmit(form) {
             ],
             filters: {},
         }, options);
-       // console.log(options.apiUrl);
+        // console.log(options.apiUrl);
         $(options.selector).dataTable({
             processing: true,
             serverSide: true,
@@ -858,5 +858,181 @@ function ajaxFormSubmit(form) {
             scrollCollapse: true,
             // dom: 'R<"top"Bf>rt<"bottom"ilp><"clear">',
         });
+    }
+}($));
+
+
+
+$.fn.dataTable.pipeline = function (opts) {
+    // Configuration options
+    var conf = $.extend({
+        pages: 5,     // number of pages to cache
+        url: '',      // script url
+        data: null,   // function or object with parameters to send to the server
+        // matching how `ajax.data` works in DataTables
+        method: 'POST' // Ajax HTTP method
+    }, opts);
+
+    // Private variables for storing the cache
+    let cacheLower = -1;
+    let cacheUpper = null;
+    let cacheLastRequest = true;
+    let cacheLastJson = null;
+
+    return function (request, drawCallback, settings) {
+        let ajax = false;
+        let requestStart = request.start;
+        let drawStart = request.start;
+        let requestLength = request.length;
+        let requestEnd = requestStart + requestLength;
+
+        if (settings.clearCache) {
+            // API requested that the cache be cleared
+            ajax = true;
+            settings.clearCache = false;
+        }
+        else if (cacheLower < 0 || requestStart < cacheLower || requestEnd > cacheUpper) {
+            // outside cached data - need to make a request
+            ajax = true;
+        }
+        else if (JSON.stringify(request.order) !== JSON.stringify(cacheLastRequest.order) ||
+            JSON.stringify(request.columns) !== JSON.stringify(cacheLastRequest.columns) ||
+            JSON.stringify(request.search) !== JSON.stringify(cacheLastRequest.search)
+        ) {
+
+            // properties changed (ordering, columns, searching)
+            ajax = true;
+        }
+
+        // Store the request for checking next time around
+        cacheLastRequest = $.extend(true, {}, request);
+
+        if (ajax) {
+            // Need data from the server
+            if (requestStart < cacheLower) {
+                requestStart = requestStart - (requestLength * (conf.pages - 1));
+
+                if (requestStart < 0) {
+                    requestStart = 0;
+                }
+            }
+
+            cacheLower = requestStart;
+            cacheUpper = requestStart + (requestLength * conf.pages);
+
+            request.start = requestStart;
+            request.length = requestLength * conf.pages;
+
+            // Provide the same `data` options as DataTables.
+            if (typeof conf.data === 'function') {
+                // As a function it is executed with the data object as an arg
+                // for manipulation. If an object is returned, it is used as the
+                // data object to submit
+                var d = conf.data(request);
+                if (d) {
+                    $.extend(request, d);
+                }
+            }
+            else if ($.isPlainObject(conf.data)) {
+                // As an object, the data given extends the default
+                $.extend(request, conf.data);
+            }
+            else if (opts.filters) {
+                $.extend(request, opts.filters);
+            }
+            //var additionalFilters = opts.filters
+            return $.ajax({
+                "type": conf.method,
+                "url": conf.url,
+                "data": request,
+                "dataType": "json",
+                "cache": false,
+                "success": function (json) {
+                    cacheLastJson = $.extend(true, {}, json);
+
+                    if (cacheLower != drawStart) {
+                        json.data?.splice(0, drawStart - cacheLower);
+                    }
+                    if (requestLength >= -1) {
+                        json.data?.splice(requestLength, json.data.length);
+                    }
+                    drawCallback(json);
+                }
+            });
+        }
+        else {
+            json = $.extend(true, {}, cacheLastJson);
+            json.draw = request.draw; // Update the echo for each response
+            json.data?.splice(0, requestStart - cacheLower);
+            json.data?.splice(requestLength, json.data.length);
+
+            //drawCallback(json);
+        }
+    }
+};
+
+// Register an API method that will empty the pipelined data, forcing an Ajax
+// fetch on the next draw (i.e. `table.clearPipeline().draw()`)
+$.fn.dataTable.Api.register('clearPipeline()', function () {
+    return this.iterator('table', function (settings) {
+        settings.clearCache = true;
+    });
+});
+
+
+//
+// DataTables initialisation
+//
+var table;
+(function ($) {
+    $.renderDataTable2 = function (options) {
+        options = $.extend({}, {
+            columns: [],
+            apiUrl: '/',
+            selector: 'table',
+            buttons: [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5'
+            ],
+            filters: {},
+        }, options);
+        // console.log(options.apiUrl);
+         table = $(options.selector).DataTable({
+            processing: true,
+            serverSide: true,
+            paging: true,
+            destroy: true,
+            dom: 'Bfrtip',
+            searching: true,
+            buttons: options.buttons,
+
+            ajax: $.fn.dataTable.pipeline({
+                url: options.apiUrl,
+                pages: 5,// number of pages to cache,
+                filters: options.filters
+            }),
+            aoColumns: options.columns,
+            //scrollY: $('[name="Applicationlist"]').offset().top + 118,
+            scrollCollapse: true,
+            drawCallback: function (settings) {
+                modifySearch();
+            }
+        });
+
+        function modifySearch() {
+            $(".dataTables_filter input")
+                .unbind()
+                .bind('keyup change', function (e) {
+                    if (e.keyCode == 13 || this.value == "") {
+                        alert('hh')
+                        console.log(table);
+                        table.search(this.value).draw();
+                    }
+                });
+        }
+
+        
     }
 }($));
